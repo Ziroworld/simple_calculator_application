8df0ea37d6a8fe1f2e1639dc190d023dcfccd0e0
meinkampf.369 <ziro0x0x0@gmail.com>
2024-11-26 16:48:31 +0545

    Added arithemetic operations.

--- lib/view/arithemetic_view.dart
+++ lib/view/arithemetic_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_calculator_application/widget/boxspace_widget.dart';
 
 class ArithemeticView extends StatefulWidget {
   const ArithemeticView({super.key});
@@ -8,8 +9,86 @@ class ArithemeticView extends StatefulWidget {
 }
 
 class _ArithemeticViewState extends State<ArithemeticView> {
+  int first = 0;
+  int second = 0;
+  int result = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) => first = int.parse(value),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "First number",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            space8y,
+            TextField(
+              onChanged: (value) => second = int.parse(value),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Second number",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            space8y,
+            Text(
+              "Result: $result",
+              style: const TextStyle(fontSize: 22),
+            ),
+            space8y,
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      result = first + second;
+                    });
+                  },
+                  child: const Text("Addition")),
+            ),
+            space8y,
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      result = first - second;
+                    });
+                  },
+                  child: const Text("Subtraction")),
+            ),
+            space8y,
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      result = first * second;
+                    });
+                  },
+                  child: const Text("Multiplication")),
+            ),
+            space8y,
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      result = (first ~/ second);
+                    });
+                  },
+                  child: const Text("Division")),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
